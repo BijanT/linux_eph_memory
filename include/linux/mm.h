bpf-fault/linux-bpf-fault

bpf_fault: add write-protect (WP) support for anonymous pages
Add write-protect fault handling to bpf_fault, providing a BPF-based
alternative to userfaultfd's UFFDIO_WRITEPROTECT functionality.

This introduces a new VM_BPF_FAULT_WP VMA flag (bit 44) that can be set
independently of VM_BPF_FAULT, allowing WP-only registrations without
requiring missing-fault interception.  The implementation reuses the
existing PTE-level uffd-wp bits (pte_uffd_wp/pte_mkuffd_wp) and
PTE_MARKER_UFFD_WP infrastructure since these are handler-agnostic.

Key components:

- VMA flag and registration: BPF_FAULT_FLAG_WP in link_create sets
  VM_BPF_FAULT_WP on the target range.  VM_BPF_FAULT and VM_BPF_FAULT_WP
  are mutually exclusive for now (combined mode is a TODO).

- Fault dispatch: do_wp_page() checks bpf_fault_pte_wp() and dispatches
  to handle_bpf_fault_wp(), which calls the BPF handle_wp_fault callback.
  On allow, re-acquires VMA lock, clears the uffd-wp PTE bit, and returns
  VM_FAULT_RETRY.

- Two writeprotect interfaces:
  * BPF kfunc bpf_fault_writeprotect() for use from struct_ops programs
  * BPF_LINK_WRITEPROTECT syscall command for userspace control
  Both call bpf_fault_wp_range() which uses change_protection() with
  MM_CP_UFFD_WP / MM_CP_UFFD_WP_RESOLVE.

- Helper separation: bpf_fault_set() now checks both VM_BPF_FAULT and
  VM_BPF_FAULT_WP (used for khugepaged exclusion, fault-around, per-page
  fidelity).  New bpf_fault_missing() checks only VM_BPF_FAULT (used at
  missing-fault dispatch sites).

- Lifecycle: teardown resolves WP markers via uffd_wp_range() before
  clearing flags.  Fork clears both VM_BPF_FAULT and VM_BPF_FAULT_WP
  in child VMAs.  BPF_LINK_WRITEPROTECT validates the link was created
  with BPF_FAULT_FLAG_WP.

Assisted-by: Claude:claude-opus-4-6

include/linux/mm.h
@@ -416,8 +416,11 @@ extern unsigned int kobjsize(const void *objp);
 #ifdef CONFIG_BPF_FAULT
 #define VM_BPF_FAULT_BIT      43
 #define VM_BPF_FAULT          BIT(VM_BPF_FAULT_BIT)
+#define VM_BPF_FAULT_WP_BIT   44
+#define VM_BPF_FAULT_WP       BIT(VM_BPF_FAULT_WP_BIT)
 #else /* !CONFIG_BPF_FAULT */
 #define VM_BPF_FAULT          VM_NONE
+#define VM_BPF_FAULT_WP       VM_NONE
 #endif /* CONFIG_BPF_FAULT */
 
 #ifdef CONFIG_64BIT

BijanT/linux_eph_memory

include/linux/mm.h
@@ -354,6 +354,7 @@ enum {
 	DECLARE_VMA_BIT(UFFD_MINOR, 41),
 	DECLARE_VMA_BIT(SEALED, 42),
 	DECLARE_VMA_BIT(BPF_FAULT, 43),
+	DECLARE_VMA_BIT(BPF_FAULT_WP, 44),
 	/* Flags that reuse flags above. */
 	DECLARE_VMA_BIT_ALIAS(PKEY_BIT0, HIGH_ARCH_0),
 	DECLARE_VMA_BIT_ALIAS(PKEY_BIT1, HIGH_ARCH_1),
@@ -514,8 +515,10 @@ enum {
 
 #ifdef CONFIG_BPF_FAULT
 #define VM_BPF_FAULT          INIT_VM_FLAG(BPF_FAULT)
+#define VM_BPF_FAULT_WP       INIT_VM_FLAG(BPF_FAULT_WP)
 #else /* !CONFIG_BPF_FAULT */
 #define VM_BPF_FAULT          VM_NONE
+#define VM_BPF_FAULT_WP       VM_NONE
 #endif /* CONFIG_BPF_FAULT */
 
 /* Bits set in the VMA until the stack is in its final location */